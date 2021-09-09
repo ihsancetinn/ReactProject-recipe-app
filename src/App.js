610feffb325e83f7c4a6986a000d5990627b3d5a
İhsan ÇETİN <ihsancetin92@gmail.com>
2021-09-09 17:39:27 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+// import Navbar from "./components/navbar/Navbar";
+import AppRouter from "./router/AppRouter";
+
+const App = () => {
+  return (
+    <AppRouter />
+  );
+};
+
+export default App;
